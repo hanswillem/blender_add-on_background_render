8c84d6bd70b3259d424204e4734620b2d719ab83
Hans Willem Gijzel <hanswillemgijzel@gmail.com>
2022-04-09 18:55:03 +0200

    Update add-on_background_render.py

--- add-on_background_render.py
+++ add-on_background_render.py
@@ -1,80 +1,89 @@
-bl_info = {
-    'name' : 'Background Render',
-    'author' : 'Hans Willem Gijzel',
-    'version' : (1, 0),
-    'blender' : (2, 80, 0  ),
-    'location' : 'View 3D > Tools > My Addon',
-    'description' : 'Creates Bat File',
-    'warning' : '',
-    'wiki_url' : '',
-    'category' : 'Render'
-    }
-
-#imports
-import bpy
+import os.path
 import subprocess
-import os
+import bpy
+
+
+bl_info = {
+    'name': 'Background Render',
+    'author': 'Hans Willem Gijzel',
+    'version': (1, 0),
+    'blender': (2, 91, 0),
+    'location': 'Properties Panel > Output > Background Render',
+    'description': 'Creates a .bat file for background rendering',
+    'warning': '',
+    'wiki_url': '',
+    'category': 'rendering'
+}
+
 
 def main_openFolder():
     subprocess.Popen('explorer ' + bpy.path.abspath('//'))
 
+
 def main_createBatFile():
     batfile = os.path.join(os.path.dirname(bpy.data.filepath), 'bg_render.bat')
     f = open(batfile, 'w+')
-    render_string = '"' + bpy.app.binary_path + '" -b "' + str(bpy.data.filepath) + '" -x 1 -a' + '\n'
+    render_string = '"' + bpy.app.binary_path + '" -b "' + \
+        str(bpy.data.filepath) + '" -x 1 -a' + '\n'
     f.write(render_string)
     f.close()
     main_openFolder()
-    
-#panel class
-class BACKGROUNDRENDER_PT_Panel(bpy.types.Panel):
-    #panel attributes
-    """Tooltip"""
-    bl_label = 'Background Render'
-    bl_idname = 'BACKGROUNDRENDER_PT_Panel'
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Background Render'
-    
-    #draw loop
-    def draw(self, context):
-        layout = self.layout
-        col = layout.column(align = True)
-        col.operator('script.bgrender_create_bat_file', text='Create .bat File')
 
-#operator class
-class CREATEBATFILE_OT_Operator(bpy.types.Operator):
-    #operator attributes
+
+# operator class
+class SCRIPT_OT_bg_render(bpy.types.Operator):
+    # operator attributes
     """Tooltip"""
-    bl_label = 'Create Bat File'
-    bl_idname = 'script.bgrender_create_bat_file'
-    
-    #poll - if the poll function returns False, the button will be greyed out
+    bl_label = 'Create a .bat file'
+    bl_idname = 'script.bg_render'
+
+    # poll - if the poll function returns False, the button will be greyed out
     @classmethod
     def poll(cls, context):
         return bpy.data.is_saved
-    
-    #execute
+
+    # execute
     def execute(self, context):
         main_createBatFile()
         return {'FINISHED'}
 
-#registration
+
+# panel class
+class VIEW_3D_PT_background_render(bpy.types.Panel):
+    # panel attributes
+    """Tooltip"""
+    bl_label = 'Background Render'
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "output"
+    bl_category = 'Background Render'
+
+    # draw loop
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column(align=True)
+        col.operator('script.bg_render', text='Create .bat file')
+
+
+# registration
 classes = (
-    BACKGROUNDRENDER_PT_Panel,
-    CREATEBATFILE_OT_Operator,
+    VIEW_3D_PT_background_render,
+    SCRIPT_OT_bg_render
 )
+
 
 def register():
     from bpy.utils import register_class
     for cls in classes:
         register_class(cls)
 
+
 def unregister():
     from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
 
-#enable to test the addon by running this script
+
+# enable to test the addon by running this script
 if __name__ == '__main__':
     register()
